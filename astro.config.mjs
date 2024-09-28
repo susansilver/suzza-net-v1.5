1d67cf31961f6bc1681a6848c6d1dd1318f45cd2
suzza <suzza@susansilver.net>
2024-09-28 10:24:05 -0700

    upload defaults

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()]
+  site: "https://suzza.net",
+  integrations: [mdx(), tailwind()],
 });
