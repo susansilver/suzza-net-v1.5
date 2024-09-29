e9f791eb17b55c6b09f1361770ba1a1a096d9a3d
suzza <suzza@susansilver.net>
2024-09-29 14:19:57 -0700

    add embeds and image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,22 +3,23 @@ import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 import favicons from "astro-favicons";
+import embeds from "astro-embed/integration";
 
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
+
+import betterImageService from "astro-better-image-service";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://suzza.net",
-  integrations: [
-    favicons({
-      masterPicture: "public/suzza-fav-icon.png",
-      emitAssets: true,
-      faviconsDarkMode: false,
-      path: "public/suzza-fav-icon.png",
-      appName: "Suzza.net",
-      appShortName: "Suzza.net",
-    }),
-    mdx(),
-    tailwind(),
-  ],
+  integrations: [favicons({
+    masterPicture: "public/suzza-fav-icon.png",
+    emitAssets: true,
+    faviconsDarkMode: false,
+    path: "public/suzza-fav-icon.png",
+    appName: "Suzza.net",
+    appShortName: "Suzza.net",
+  }), embeds(), mdx(), tailwind(), sitemap(), betterImageService()],
 });
