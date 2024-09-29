5aa1cc955854306670af5bea7d019082099bfc18
suzza <suzza@susansilver.net>
2024-09-29 14:11:28 -0700

    ass astro fav icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,23 @@
 import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
+import favicons from "astro-favicons";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://suzza.net",
-  integrations: [mdx(), tailwind()],
+  integrations: [
+    favicons({
+      masterPicture: "public/suzza-fav-icon.png",
+      emitAssets: true,
+      faviconsDarkMode: false,
+      path: "public/suzza-fav-icon.png",
+      appName: "Suzza.net",
+      appShortName: "Suzza.net",
+    }),
+    mdx(),
+    tailwind(),
+  ],
 });
