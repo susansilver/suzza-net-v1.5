b2214357af31bb521e85bf18536683ad7b477caa
suzza <suzza@susansilver.net>
2024-09-29 14:22:47 -0700

    add compressors integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,10 @@ import sitemap from "@astrojs/sitemap";
 
 import betterImageService from "astro-better-image-service";
 
+import playformCompress from "@playform/compress";
+
+import compressor from "astro-compressor";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://suzza.net",
@@ -21,5 +25,11 @@ export default defineConfig({
     path: "public/suzza-fav-icon.png",
     appName: "Suzza.net",
     appShortName: "Suzza.net",
-  }), embeds(), mdx(), tailwind(), sitemap(), betterImageService()],
+  }), embeds(), mdx(), tailwind(), sitemap(), betterImageService(), (await import("@playform/compress")).default({
+    CSS: true,
+    HTML: true,
+    Image: false,
+    JavaScript: true,
+    SVG: false,
+  }), compressor()],
 });
